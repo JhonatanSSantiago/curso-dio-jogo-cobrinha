7ec4858e7e6918c749f196d287521fe2f9399ca7
Jhonatan Santiago <jhonssantiago@gmail.com>
2020-12-11 09:29:12 -0300

    definindo movimento pelo teclado

--- main.js
+++ main.js
@@ -23,6 +23,23 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right"){
+        direction= "left";
+    }
+    if(event.keyCode == 38 && direction != "down"){
+        direction= "up";
+    }
+    if(event.keyCode == 39 && direction != "left"){
+        direction= "right";
+    }
+    if(event.keyCode == 40 && direction != "up"){
+        direction= "down";
+    }
+}
+
 function iniciarJogo(){
     criarBG();
     criarCobrinha();
@@ -46,3 +63,4 @@ function iniciarJogo(){
 }
 
 let jogo = setInterval(iniciarJogo, 100);
+
